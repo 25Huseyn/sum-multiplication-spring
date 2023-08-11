14299657931bf0a71f696fbcd8e9a50f48e3c608
25Huseyn <huseynajafov@gmail.com>
2023-08-11 19:44:51 +0400

    Task01

--- src/main/java/com/example/task01spring/OperationsController.java
+++ src/main/java/com/example/task01spring/OperationsController.java
@@ -9,19 +9,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/operations")
 public class OperationsController {
-    @GetMapping
+    @GetMapping("/sum")
     public String getSum(Model model,
                          @RequestParam("a") int a,
                          @RequestParam("b") int b) {
         model.addAttribute("a", a);
         model.addAttribute("b", b);
         int sum = a + b;
-        int multiplication = a * b;
-        int subtraction = a - b;
-        int division = a / b;
         model.addAttribute("sum", sum);
+        return "nums";
+    }
+
+    @GetMapping("/multiplication")
+    public String getMultiplication(Model model,
+                                    @RequestParam("a") int a,
+                                    @RequestParam("b") int b) {
+        model.addAttribute("a", a);
+        model.addAttribute("b", b);
+        int multiplication = a * b;
         model.addAttribute("multiplication", multiplication);
+        return "nums";
+    }
+
+    @GetMapping("/subtraction")
+    public String getSubtraction(Model model,
+                                 @RequestParam("a") int a,
+                                 @RequestParam("b") int b) {
+        model.addAttribute("a", a);
+        model.addAttribute("b", b);
+        int subtraction = a - b;
         model.addAttribute("subtraction", subtraction);
+        return "nums";
+    }
+
+    @GetMapping("/division")
+    public String getDivision(Model model,
+                              @RequestParam("a") int a,
+                              @RequestParam("b") int b) {
+        model.addAttribute("a", a);
+        model.addAttribute("b", b);
+        int division = a / b;
         model.addAttribute("division", division);
         return "nums";
     }
