686aeaaa416cab7dff3fe54b7617f2ea7085a2d4
25Huseyn <huseynajafov@gmail.com>
2023-08-11 19:25:57 +0400

    Task01

--- src/main/java/com/example/task01spring/OperationsController.java
+++ src/main/java/com/example/task01spring/OperationsController.java
@@ -11,14 +11,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class OperationsController {
     @GetMapping
     public String getSum(Model model,
-                         @RequestParam(name = "a") int a,
-                         @RequestParam(name = "b") int b) {
+                         @RequestParam("a") int a,
+                         @RequestParam("b") int b) {
         model.addAttribute("a", a);
         model.addAttribute("b", b);
         int sum = a + b;
         int multiplication = a * b;
+        int subtraction = a - b;
+        int division = a / b;
         model.addAttribute("sum", sum);
         model.addAttribute("multiplication", multiplication);
+        model.addAttribute("subtraction", subtraction);
+        model.addAttribute("division", division);
         return "nums";
     }
 }
